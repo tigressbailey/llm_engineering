1af6225a627761701d6f66f3a78f541a4cc031e7
Bailey Zhang <tigressbailey@gmail.com>
2026-06-20 11:21:58 +0200

    feat: week5 finished

--- week5/implementation/ingest.py
+++ week5/implementation/ingest.py
@@ -2,7 +2,7 @@ import os
 import glob
 from pathlib import Path
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter, MarkdownTextSplitter
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_openai import OpenAIEmbeddings
@@ -10,17 +10,17 @@ from langchain_openai import OpenAIEmbeddings
 
 from dotenv import load_dotenv
 
+
+load_dotenv(override=True)
+
+
 MODEL = "gpt-4.1-nano"
 
 DB_NAME = str(Path(__file__).parent.parent / "vector_db")
 KNOWLEDGE_BASE = str(Path(__file__).parent.parent / "knowledge-base")
 
-embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-# embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
-
-load_dotenv(override=True)
-
-
+# embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 
 
 def fetch_documents():
@@ -40,6 +40,7 @@ def fetch_documents():
 
 def create_chunks(documents):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=200)
+    # text_splitter = MarkdownTextSplitter()
     chunks = text_splitter.split_documents(documents)
     return chunks
 
